e3c240d9fe8e9f7516d2cce21205baa0acb8da5e
Elemar Deckmann <contato@elemar.eng.br>
2021-08-05 00:16:08 +0000

    First Commit

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,10 @@
+export default class Stock {
+    id!: number
+    name!: string
+    price!: number
+    minimum!: number
+    exclusiveMinimum!: boolean
+    variation!: number
+    date!: string
+
+}
